981dbfe4b9fc883acfe429a38854b7ca042b3765
Rachel Varon <rkvaron@gmail.com>
2018-03-22 11:42:56 -0400

    Tinkered and tinkered

--- app.js
+++ app.js
@@ -2,27 +2,22 @@
 const overlay = document.getElementById('overlay');
 const phrase = document.getElementById('phrase');
 const qwerty = document.getElementById('qwerty');
-const keyboardButtons = qwerty.getElementsByTagName("button");
 let missed = 0;
 
-//event to hide the start screen overlay
-overlay.addEventListener('click', (e) => {
-  overlay.style.visibility="hidden";
-  // to show: overlay.style.visibility="visible";
-});
+
 
 //phrases array with 5 strings, letters and spaces only, no punctuation or special characters
-var phrases = ["deal or no deal", "survey says", "this is jeopardy", "i would like to buy a vowel", "the price is right"];
+const phrases = ["deal or no deal", "survey says", "this is jeopardy", "i would like to buy a vowel", "the price is right"];
 
 //generate a random number between 1 and phrases.length
 function randomNumber(){
-  return Math.floor(Math.random() * phrases.length) + 1;
+  return Math.floor(Math.random() * phrases.length);
 }
 
-//random function selector:
+//function to select a random phrase from the array and split it into an array of characters
 function getRandomPhraseAsArray(arr){
   //selects one of the phrases
-  let phraseIndex = randomNumber() - 1;
+  let phraseIndex = randomNumber();
   let answerPhrase = arr[phraseIndex];
 //separates the letters into an array
   const answerArray= answerPhrase.split("");;
@@ -30,8 +25,8 @@ function getRandomPhraseAsArray(arr){
   return answerArray;
 }
 
-//function to select random phrase  and add to the gameboard
-function addPhraseToDisplay(arr){
+//function to select random phrase and add to the gameboard, than add classes of "letter" or "space"
+function addPhraseToDisplay(arr) {
   const ul = phrase.firstElementChild;
   for(let i=0; i < arr.length; i+=1){
     let li = document.createElement('li');
@@ -45,62 +40,86 @@ function addPhraseToDisplay(arr){
   }
 }
 
+//function to check button clicked to phrase. returns either the letter or null
+function checkLetter(button) {
+  //as a default, it returns null
+  let letterFound = null;
+
+  //function should loop over letter to check if they match the letter in the phraseLetters
+  for(let i = 0; i < phraseLetters.length; i ++) {
+    if( phraseLetters[i].textContent == button.textContent ) {
+      //if there is a match, function should add class = "show" to the item containing the letter, store the matching letter inside letterFound
+      phraseLetters[i].classList.add("show");
+      letterFound = phraseLetters[i].textContent;
+    }
+  }
+  return letterFound;
+}
+
+//checks if the game is won, lost or keep playing
+function checkWin(){
+  const numberCorrect = document.querySelectorAll('.show');
+  if(phraseLetters.length === numberCorrect.length) {
+    overlay.classList.replace('start', 'win');
+    const h2 = document.createElement('h2');
+    h2.textContent = "You win!";
+    overlay.appendChild(h2);
+    const restartButton = overlay.querySelector('a');
+    restartButton.innerHTML="Play Again?";
+    overlay.insertBefore(h2, restartButton);
+    overlay.style.visibility="visible";
+
+  } else if (missed === 5 ) {
+    overlay.classList.replace('start', 'lose');
+    const h2 = document.createElement('h2');
+    h2.textContent = "You Lose!";
+    overlay.appendChild(h2);
+    const restartButton = overlay.querySelector('a');
+    restartButton.innerHTML="Play Again?";
+    overlay.style.visibility="visible";
+  } else {
+    console.log("Keep guessing");
+  }
+}
+
+//to restart the game
+function restartGame() {
+  location.reload();
+}
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
 const phraseLetters = document.querySelectorAll('.letter');
 
-//checkLetter function: to be used in the event addEventListener
-//should have 1 parameter: the clicked button by player
-//function should loop over letter to check if they match the letter in the keyboardButtons
-//if there is a match, function should add class = "show" to the item containing the letter, store the matching letter inside a variable and return that letter.
 
 
-
-qwerty.addEventListener('click', (event) => {
-  //assign each button its textContext
-  const buttonLetter = event.target.textContent;
-  const isClicked = event.target;
-  if (isClicked && buttonLetter.tagName == "button") {
-    event.target.classList.add("chosen");
-  } else {
-    console.log("no dice");
-  }
-
-    for(let i=0; i < phraseLetters.length; i+=1) {
-
-      if( phraseLetters[i].textContent == buttonLetter ) {
-        let letterFound = phraseLetters[i];
-        letterFound.classList.add("show");
-      } else {
-        console.log('nope');
-        //do I add a return null; here? I think it breaks the conditional
-      }
+qwerty.addEventListener('click', (e) => {
+  if (e.target.tagName == "BUTTON") {
+    const button = e.target;
+    button.classList.add("chosen");
+    let letterFound = checkLetter(button);
+    if (letterFound !== null) {
+      button.disabled = true;
+    } else {
+      //hearts turn grey with a miss and adjusts score
+      const tries = document.querySelectorAll('.tries');
+      const heart = tries[missed].firstElementChild;
+      heart.setAttribute("src", "images/lostHeart.png");
+      missed += 1;
     }
+  checkWin();
+  }
 });
 
+//event to hide the start screen overlay
+overlay.addEventListener('click', (e) => {
+  overlay.style.visibility="hidden";
+  // to show: overlay.style.visibility="visible";
+});
 
-
-
-
-
-
-
-
-
-//backup stuff
-
-
-//missed +=1;
-
-// qwerty.addEventListener('click', (event) => {
-//   //assign each button its textContext
-//   let buttonLetter = event.target.textContent;
-//   for(let i=0; i < phraseLetters.length; i+=1) {
-//     if( phraseLetters[i].textContent == buttonLetter ) {
-//       let letterInPhrase = phraseLetters[i];
-//       letterInPhrase.classList.add("show");
-//     } else {
-//       console.log("nope");
-//     }
-//   }
-// });
+overlay.addEventListener('click', (e) => {
+  if(e.target.textContent === "Play Again?"){
+    restartGame();
+  }
+});
